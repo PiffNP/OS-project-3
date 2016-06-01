6104f3e772911467417945bd9b441509e3752752
Zhang Yiwei <zhangyiwei1234567@126.com>
2016-06-01 22:36:24 +0800

    change format

--- test.py
+++ test.py
@@ -11,7 +11,6 @@ cfg = json.load(open('conf/settings.conf'))
 
 server_url = cfg['primary'] + ":" + cfg['port']
 
-
 insert_url = "/kv/insert/key={0}&value={1}"
 query_url = "/kv/get/?key={0}"
 update_url = "/kv/update/key={0}&value={1}"
@@ -27,7 +26,7 @@ class Test:
     suc_insert_num = 0
     result_flag = 'success'
 
-    def request(self, method_str, request_str, request_type=None,expect_dict=None):
+    def request(self, method_str, request_str, request_type=None, expect_dict=None):
         def func(request):
             if request_type == 'insert':
                 self.total_insert_num += 1
@@ -48,9 +47,9 @@ class Test:
                 print("request {}".format(request_str))
             res = conn.getresponse()
             res_json = json.loads(res.read().decode('utf-8'))
-            if expect_dict is not None and len(set(res_json)^set(expect_dict))!=0:
-                print("failed at {}!={}".format(expect_dict,res_json))
-                self.result_flag='fail'
+            if expect_dict is not None and len(set(res_json) ^ set(expect_dict)) != 0:
+                print("failed at {}!={}".format(expect_dict, res_json))
+                self.result_flag = 'fail'
             # maybe we should convert the value to a unicode string before output it
             if len(sys.argv) == 1 and sys.argv[0] == '-d':
                 print("{}:{}".format(request_str, res_json))
@@ -100,56 +99,56 @@ class Test:
                       self.get_statistic[round(len(self.get_statistic) * 0.9)]))
 
     def multiple_key_test(self):
-        keys = ["multiple_"+str(i) for i in range(256)]
         # os.system("bin//start_server -p")
         # os.system("bin//start_server -b")
         time.sleep(1)
         for key in keys:
+            time.sleep(0.005)
             self.request("POST", insert_url.format(key, key), 'insert')
-        time.sleep(1)
         for key in keys:
+            time.sleep(0.005)
             self.request("GET", query_url.format(key), 'get')
-        time.sleep(1)
         for key in keys:
+            time.sleep(0.005)
             self.request("POST", update_url.format(key, key + key))
-        time.sleep(1)
         for key in keys:
+            time.sleep(0.005)
             self.request("GET", query_url.format(key), 'get')
-        time.sleep(1)
         for key in keys:
-            self.request("POST",delete_url.format(key),'delete')
+            time.sleep(0.005)
+            self.request("POST", delete_url.format(key), 'delete')
         time.sleep(1)
 
         # os.system("bin//stop_server -p")
         # os.system("bin//stop_server -b")
         # request("GET","/kvman/countkey")
         # request("GET","/kvman/dump")
+
     def single_key_pressure_test(self):
         time.sleep(1)
-        key="single_pressure"
-        value="init_val"
-        iteration_time=1000
-        self.request("POST",insert_url.format(key,value),'insert')
+        key = "single_pressure"
+        value = "init_val"
+        iteration_time = 1000
+        self.request("POST", insert_url.format(key, value), 'insert')
 
         for i in range(iteration_time):
             time.sleep(0.005)
-            self.request("POST",update_url.format(key,str(i)),'update')
+            self.request("POST", update_url.format(key, str(i)), 'update')
             time.sleep(0.005)
-            self.request("GET",query_url.format(key),'get',expect_dict={'success':'true','value':str(i)})
-
+            self.request("GET", query_url.format(key), 'get', expect_dict={'success': 'true', 'value': str(i)})
         time.sleep(1)
 
     def key_delete_test(self):
         time.sleep(1)
         key = 'delete_test'
-        value="delete_val"
-        iteration_time=200
+        value = "delete_val"
+        iteration_time = 200
 
         for i in range(iteration_time):
             time.sleep(0.005)
-            self.request("POST",insert_url.format(key,value),'insert')
+            self.request("POST", insert_url.format(key, value), 'insert')
             time.sleep(0.005)
-            self.request("POST",delete_url.format(key),'delete')
+            self.request("POST", delete_url.format(key), 'delete')
         time.sleep(1)
 
 
